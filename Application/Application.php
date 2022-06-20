598cbfe07c5fd3cacaac13c9841517f4adf06761
khalil <kalilovsky@gmail.com>
2022-06-20 23:40:04 +0200

    feat : add relative path for all the file

--- Application/Application.php
+++ Application/Application.php
@@ -13,15 +13,25 @@ class Application
      *
      * @return void
      */
-    public function start(): void
+    public static function start(): void
     {   
-        echo ("Bonjour vous êtes sur l'outil de conversion d'un fichier avec données brut en fichier CSV \n");
+        echo ("Bonjour vous êtes sur l'outil de conversion d'un fichier avec données brut en fichier CSV". PHP_EOL);
+        echo "Veuillez entrer le chemin complet du fichier métadonnées..." . PHP_EOL;
+        $metaDataFilePath = readline();
+        echo "Veuillez entrer le chemin complet du fichier que vous voulez convertir..." . PHP_EOL;
+        $rawDataFilePath = readline();
+        echo "Veuillez entrer le chemin ou vous voulez enregistrez le fichier CSV de sortie..." . PHP_EOL;
+        $csvFilePath = readline();
+        // /home/khalil/portfolio/Projet_13/testtechnique/meta.csv
+        // /home/khalil/portfolio/Projet_13/testtechnique/data.txt
+        // /home/khalil/portfolio/Projet_13/testtechnique/data.csv
         try{
             $metaData = new MetaDataParser();
-            $metaData->parseMetaDataFromFile("/home/khalil/portfolio/Projet_13/testtechnique/meta.csv");
-            $dataParser = new DataParser("/home/khalil/portfolio/Projet_13/testtechnique/data.txt","/home/khalil/portfolio/Projet_13/testtechnique/data.csv",$metaData);
+            $metaData->parseMetaDataFromFile($metaDataFilePath);
+            $dataParser = new DataParser($rawDataFilePath,$csvFilePath,$metaData);
             $dataParser->startParse();
             echo('Traitement fini'.PHP_EOL);
+            echo('Votre fichier est disponible au chemin suivant : ' . PHP_EOL . $csvFilePath . PHP_EOL);
         } catch (Exception $e){
             echo 'Exception reçue : ' . $e->getMessage() . PHP_EOL;
         }
--- data_to_csv.php
+++ data_to_csv.php
@@ -1,6 +1,5 @@
 <?php
 require_once("Librairies/autoload.php");
 
-$application = new Application();
-$application->start();
+Application::start();
 
